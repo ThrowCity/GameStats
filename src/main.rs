d636e90b662a27b9c71a9fc40e4ba91385543a5e
mqxf <mqxf@mvteam.dev>
2024-11-05 02:32:27 +0100

    csv parser, data processor, csv output

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
+use hashbrown::HashMap;
+use crate::game::*;
+use crate::player::Player;
+use crate::team::*;
+
+pub mod game;
+pub mod team;
+pub mod player;
+
+fn read(path: &str) -> String {
+    let mut file = OpenOptions::new().read(true).open(path).unwrap();
+    let mut s = String::new();
+    file.read_to_string(&mut s).unwrap();
+    s
+}
+
+fn main() {
+    let teams = parse_teams(read("data/teams.csv"));
+
+    let mut players: HashMap<String, Player> = teams.values().flat_map(|team| {
+        team.players.iter().cloned().map(|p| (p.to_lowercase().clone(), Player::new(p, team.name.clone())))
+    }).filter(|p| !p.1.name.is_empty()).collect();
+
+    for game in fs::read_dir("data/matches/").unwrap() {
+        let game = game.unwrap();
+        let path = game.path().to_str().unwrap().to_string();
+        let data = parse_game(&path, read(&path));
+        apply(&mut players, &teams, data);
+    }
+
+    let mut s = Player::csv_header();
+
+    for p in players.values_mut() {
+        p.finish();
+        s.push_str(&p.to_csv());
+        s.push('\n');
+    }
+
+    let mut output = OpenOptions::new().write(true).truncate(true).create(true).open("data/stats.csv").unwrap();
+    write!(&mut output, "{}", s).unwrap();
+}
